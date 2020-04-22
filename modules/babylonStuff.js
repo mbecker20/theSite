a568b942adb676843c052542ffbd0e952581417b
Max Becker <becker.maxh@gmail.com>
2020-04-22 06:27:46 -0700

    tweak

--- modules/babylonStuff.js
+++ modules/babylonStuff.js
@@ -422,7 +422,7 @@ class Cam {
     static JOYSTICKMOVEMULT() {return .05} // delta target step = joystickmovemult * (stickpos - centerpos)
     static JOYSTICKMOVEINTERPMULT() {return .3}
 
-    static JOYSTICKROTMULT() {return .1}
+    static JOYSTICKROTMULT() {return .008}
     static JOYSTICKROTINTERPMULT() {return .3}
 
     static MakeCam(camPos, scene, canvas, engine) {
